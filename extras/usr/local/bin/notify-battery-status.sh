bee01f7f4b05c1d2dff260ace6db8f7b44d2ee27
critiqjo <john.ch.fr@gmail.com>
2016-10-17 23:31:02 +0530

    Battery critical notification

--- extras/usr/local/bin/notify-battery-status.sh
+++ extras/usr/local/bin/notify-battery-status.sh
@@ -6,5 +6,9 @@ env > $HOME/.cache/udev-battery-env
 capacity=$POWER_SUPPLY_CAPACITY
 energy_now=$POWER_SUPPLY_ENERGY_NOW
 energy_full=$POWER_SUPPLY_ENERGY_FULL
+urgency="normal"
+
+[[ "$POWER_SUPPLY_STATUS" == "Discharging" && "$capacity" -le 20 ]] && urgency="critical"
+
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
-notify-send "Battery: ${capacity}% ( $((energy_now / 1000)) / $((energy_full / 1000)) )"
+notify-send --urgency=$urgency "Battery: ${capacity}% ( $((energy_now / 1000)) / $((energy_full / 1000)) )"
